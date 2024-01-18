d9157b29c6bef8f9bc5868a7358d6b99fe43bcde
Abdullah jubayer <abdullahjubayer783@gmail.com>
2024-01-18 06:37:39 +0600

    template error

--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-CSRF_TRUSTED_ORIGINS = ['https://smart-care-1nw7.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://papri-shop.onrender.com','https://*.127.0.0.1']
 SECRET_KEY = 'django-insecure-9xq73l3mxwd@*!@w^df!zthcwxmlsu_gb9aajh9y9tm5s3x#g^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
